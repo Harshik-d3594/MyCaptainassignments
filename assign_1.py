bc84cf4c4c5a83ee3b31d1682b439986c04579c7
Harshik-d3594 <97084781+Harshik-d3594@users.noreply.github.com>
2022-01-30 15:21:25 +0530

    Add files via upload

---
+++ assign_1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 30 15:18:14 2022
+
+@author: Agroveggies
+"""
+
+#assignment 1 :
+    
+ #area of circle 
+
+ pi = 3.141592
+ r = float(input("Enter the radius of the circle : "))
+ area = pi*r*r
+ print("Area of the cirle : " ) 
+ print(area)   
+    
+ #filename extension
+
+ x = str(input("what is the filename : "))
+
+ print("the extension of the file is : python")   
+    
+    
